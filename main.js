674ae8fc6736d7cf11ecaff83d5eadb65d3eb591
Viacheslav-Kaliberda <87283585+Viacheslav-Kaliberda@users.noreply.github.com>
2022-06-04 00:26:41 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+
+
+ function abc(){
+    let x=document.getElementById("pole1").value;
+    let y=document.getElementById("pole2").value;
+    {
+        document.getElementById("oblicz").innerHTML="Dzięki kruszarce zaoszczędzisz "+ ((x*y)-(x*y)/5) +" rocznie."
+    }
+            }
